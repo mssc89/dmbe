3f4c68ae6077ad25b0ffe157c944ca0d51ef91c6
mssc <oweziz89@gmail.com>
2020-02-25 09:03:22 +0100

    Basic skeleton and Clarity css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './parts/header/header.component';
+import { FooterComponent } from './parts/footer/footer.component';
+import { SidenavComponent } from './parts/sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './parts/post/post.component';
+import { PostFullComponent } from './parts/post-full/post-full.component';
+import { PostListComponent } from './parts/post-list/post-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    HomeComponent,
+    PostComponent,
+    PostFullComponent,
+    PostListComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
