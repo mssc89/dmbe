3f2eaf35152f7c7f3d4460098e175dc9d9b9f6d5
mssc <oweziz89@gmail.com>
2020-02-25 09:03:22 +0100

    Basic skeleton and Clarity css

---
+++ src/app/parts/post-full/post-full.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-full',
+  templateUrl: './post-full.component.html',
+  styleUrls: ['./post-full.component.scss']
+})
+export class PostFullComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
