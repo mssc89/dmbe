40d43e31b99e21d2a21f42e9e4f72d57fe7ce874
mssc89 <oweziz89@gmail.com>
2020-03-07 16:20:15 +0100

    Revert "move posts component to blog module"
    This reverts commit 59912b49a6c65c7f28b576120c70a3de5012d473.

--- src/app/blog/posts/posts.component.ts
+++ src/app/blog/posts/posts.component.ts
@@ -1,26 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
-import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-
 export class PostsComponent implements OnInit {
 
-  posts: Post[] = [];
-
-  constructor(private api: ApiService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.api.getPosts().subscribe(res => {
-      this.posts = res;
-    }, err => {
-      console.log(err);
-    });
-
   }
 
 }
