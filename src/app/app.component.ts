08f82decdcda20a2f1b1f25112663c2ef0099a1b
mssc89 <oweziz89@gmail.com>
2020-08-10 00:48:50 +0200

    Added translation support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalService } from './services/modal.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,15 @@ import { ModalService } from './services/modal.service';
 })
 export class AppComponent {
 
-  constructor(private modal: ModalService){}
+  languages = [
+    { 'name':'English', 'abb':'en' },
+    { 'name':'Polish', 'abb':'pl' }
+  ]; // test translation list, it should be received from the API
+
+  constructor(private modal: ModalService, public translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 
   login(){
     this.modal.toggle('login', true);
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 import { ClarityModule } from '@clr/angular';
+
 import { TruncateModule } from '@yellowspot/ng-truncate';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [],
@@ -12,7 +21,15 @@ import { TruncateModule } from '@yellowspot/ng-truncate';
     FormsModule,
     HttpClientModule,
     ClarityModule,
-    TruncateModule
+    TruncateModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+      }
+    })
   ],
   exports: [
     CommonModule,
