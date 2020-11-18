f7ce4c5d115c721685bbc122abb2a5151c3c0e67
mssc89 <oweziz89@gmail.com>
2020-11-18 12:05:12 +0100

    Updated angular to 11

--- src/app/modules/blog/brief/brief.component.spec.ts
+++ src/app/modules/blog/brief/brief.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BriefComponent } from './brief.component';
 
@@ -6,7 +6,7 @@ describe('BriefComponent', () => {
   let component: BriefComponent;
   let fixture: ComponentFixture<BriefComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BriefComponent ]
     })
--- src/app/modules/blog/posts/posts.component.spec.ts
+++ src/app/modules/blog/posts/posts.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PostsComponent } from './posts.component';
 
@@ -6,7 +6,7 @@ describe('PostsComponent', () => {
   let component: PostsComponent;
   let fixture: ComponentFixture<PostsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PostsComponent ]
     })
